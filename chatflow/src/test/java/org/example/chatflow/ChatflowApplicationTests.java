ae0694414c1b3fd2cdfaffdc19764483899754c2
zerui.zeng <zengzerui@keyfil.com>
2025-10-21 10:27:25 +0800

    feat：后端工程初始化

---
+++ chatflow/src/test/java/org/example/chatflow/ChatflowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chatflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
